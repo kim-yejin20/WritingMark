ab0ce3f068d3955e150da40143ef6118a2430dc5
kim-yejin20 <jin11241124@gmail.com>
2022-02-19 22:11:53 +0900

    [Fix]moment 수정

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,7 @@
 import crypto from './crypto';
 import errorGenerator from './errorGenerator';
 import bcrypt from './bcrypt';
-import moment from './moment';
 import jwt from './jwt';
 import aws from './aws';
 
-export { crypto, errorGenerator, bcrypt, moment, jwt, aws };
+export { crypto, errorGenerator, bcrypt, jwt, aws };
